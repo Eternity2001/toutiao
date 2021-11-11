7fc0a37b3688e0829cc1d8a2b26651c96f8a211b
Eternity <2901174640@qq.com>
2021-11-11 10:17:18 +0800

    px转换rem

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,27 @@
   <div id="#app">
     <h1><i class="toutiao icon-shoucang"></i>黑马头条</h1>
     <h2>一剑独尊</h2>
-    <h3>一刀砍死你</h3>
-    <router-view/>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-cell center title="标题">
+      <template #right-icon>
+        <van-switch v-model="checked" size="24"/>
+      </template>
+    </van-cell>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      checked: true
+    }
+  }
 }
 </script>
 
